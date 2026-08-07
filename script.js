43b77f0b4aea6a32d84f3971a740c7ab7c4fc738
OUPS! Festival <manuelbischof.phil@gmail.com>
2026-08-07 12:40:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -269,7 +269,7 @@ credits:""
 image:"images/Card_30.png",
 title:"SUBJECT 30",
 narrative:"I will fly anyway, whatever you will say.",
-text:`But you don’t need wings to fly.
+text:`But you don't need wings to fly.
 
 You can fly in many different ways. Sometimes, when the pain takes over, it becomes difficult to see other possibilities. Our view becomes smaller, and we forget that there are still many directions we can go.
 
